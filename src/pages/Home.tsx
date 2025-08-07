4e9c2094b31a68040c6665c56949e92edfbb0317
pujialimin <tbr1dashboard@gmail.com>
2025-08-07 11:37:27 +0700

    update path gambar logo di halaman Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen -mt-20">
-      <img
-        src="/public/homes.png" // Ganti dengan path file gambar kamu
-        alt="App Logo"
-        className="w-900 h-40 mb-20" // Ukuran logo (bisa disesuaikan)
-      />
+      <img src="/homes.png" 
+      alt="App Logo" 
+      className="w-900 h-40 mb-20" />
     </div>
   );
 }
