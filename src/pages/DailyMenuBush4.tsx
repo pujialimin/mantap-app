dd7868b8df1fdfab8a8b2edfc6c67e6536f84a69
pujialimin <tbr1dashboard@gmail.com>
2025-10-17 10:41:33 +0700

    edit lebar kolom

--- src/pages/DailyMenuBush4.tsx
+++ src/pages/DailyMenuBush4.tsx
@@ -53,6 +53,7 @@ const columnWidths: Record<string, string> = {
   date_in: 'min-w-[0px]',
   doc_status: 'min-w-[30px]',
 
+  tracking_sp: 'min-w-[200px]',
   priority: 'min-w-[00px]',
   status_pe: 'min-w-[0px]',
   cek_sm4: 'min-w-[0px]',
