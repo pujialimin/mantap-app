93e62686b58afcc22c7e25a35894d4a70ded9552
pujialimin <tbr1dashboard@gmail.com>
2025-10-17 10:00:26 +0700

    update all

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -179,7 +179,7 @@ export default function Sidebar({ isCollapsed }: { isCollapsed: boolean }) {
       {/* Version Info di pojok kiri bawah */}
       {!isCollapsed && (
         <div className="absolute bottom-6 left-2 text-xs text-[#f0f0f0]">
-          Contact: Puji.Ali/TBR-1
+          Contact: Puji.Ali
         </div>
       )}
     </div>
